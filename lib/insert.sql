2b5a923f19ea6316f209b52aa7d4838a4961f92d
Sam <sam@Sams-MacBook-Air.local>
2021-02-07 21:34:57 -0500

    commit tree 98cc190db442b8b4a1b8e2123b2bed764ed8ba46

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 4, "M", "Brown", "Docile", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 3, "M", "Black", "Excitable", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 3, "F", "Brown", "Docile", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 5, "F", "Brown", "Curious", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 2, "F", "Black", "Lazy", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 8, "M", "Black", "Mean", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 2, "F", "Black", "Docile", false);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 4, "M", "Brown", "Mean", false);
